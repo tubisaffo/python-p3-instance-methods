17567a7971e1ce5765889172071013bfcf8d02cb
tubisaffo <tubisaffo@gmail.com>
2024-05-26 22:54:25 +0300

    instances

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,15 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
 
+        
+
+    def sit(self):
+        print("The dog is sitting.")    
+
+fido = Dog()
+fido.bark()
     #Instance method definition
-    pass
+   
